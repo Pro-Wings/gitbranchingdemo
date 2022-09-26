f4ecb9e13a763faf188797f29ffb80e1d52b150d
JD <jayprowings@gmail.com>
2022-09-26 12:21:50 +0530

    updated multiplication

--- src/main/java/gitbranchingdemo/A.java
+++ src/main/java/gitbranchingdemo/A.java
@@ -5,7 +5,7 @@ public class A {
 	public static void main(String[] args) {
 		
 		
-		add(10,20);
+		mul(10,20);
 		
 	}
 	
@@ -23,6 +23,7 @@ public class A {
 	public static void mul(int a, int b)
 	{
 		System.out.println(a * b);
+		//alskdjlksa
 	}
 
 }
