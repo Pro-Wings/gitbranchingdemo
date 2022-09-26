716737388a6b96d250dcd72017b50ace5d599814
JD <jayprowings@gmail.com>
2022-09-26 11:53:44 +0530

    initial draft

---
+++ src/main/java/gitbranchingdemo/A.java
@@ -0,0 +1,17 @@
+package gitbranchingdemo;
+
+public class A {
+	
+	public static void main(String[] args) {
+		
+		
+		add(10,20);
+		
+	}
+	
+	public static void add(int a, int b)
+	{
+		System.out.println(a + b);
+	}
+
+}
